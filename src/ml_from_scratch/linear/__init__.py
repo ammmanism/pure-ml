c0fb73a2413c05300c46e22ae971b84d250ad993
ammmanism <amman.khanansari@gmail.com>
2026-04-13 12:41:58 +0530

    refactor: library cleanup and __init__ files for modularity

---
+++ src/ml_from_scratch/linear/__init__.py
@@ -0,0 +1,10 @@
+from .linear_regression import LinearRegression
+from .logistic_regression import LogisticRegression
+from .regularization import l1_penalty, l2_penalty
+
+__all__ = [
+    "LinearRegression",
+    "LogisticRegression",
+    "l1_penalty",
+    "l2_penalty",
+]
---
+++ src/ml_from_scratch/math/__init__.py
@@ -0,0 +1,10 @@
+from .linear_algebra import power_iteration, gaussian_elimination, svd
+from .probability import normal_pdf, sample_normal
+
+__all__ = [
+    "power_iteration",
+    "gaussian_elimination",
+    "svd",
+    "normal_pdf",
+    "sample_normal",
+]
